6e6327b8560a408826cca6a8b7efdca722091918
bjmt <benjmtremblay@gmail.com>
2020-06-01 14:33:56 +0200

    update to match current figure

--- CdiffTree/figs.R
+++ CdiffTree/figs.R
@@ -36,7 +36,8 @@ md2B <- dplyr::left_join(mdB, md2, by = c("Acc" = "Acc"))
 md2A <- md2A[!duplicated(md2A$Assembly), ]
 md2B <- md2B[!duplicated(md2B$Assembly), ]
 
-cols <- RColorBrewer::brewer.pal(12, "Paired")
+# cols <- RColorBrewer::brewer.pal(13, "Paired")
+cols <- c("black","red","blue","orange","dark green","green","purple","cyan","pink","brown","gray","dark blue","dark cyan","white")
 
 # TOXIN A - HIGH STRINGENCY -------------------------------------------------
 
@@ -49,18 +50,6 @@ tipsA1 <- data.frame(
   SubToxin = tipsA1$ToxinSubtype
 )
 
-p <- gheatmap(
-  ggtree(tree, branch.length = "none", layout = "circular"),
-  tipsA1, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin A"
-) +
-  scale_fill_manual(
-    name = "Toxin A",
-    values = c(cols[c(2, 4, 6, 8, 10)], "#000000", "#999999")
-  ) +
-  ggtitle("Toxin A - High Stringency")
-
-ggsave("figs/ToxinA_HighStringency.pdf", p)
-
 # TOXIN A - LOW STRINGENCY -------------------------------------------------
 
 tipsA2 <- md2A
@@ -70,18 +59,6 @@ tipsA2 <- data.frame(
   row.names = tipsA2$Assembly,
   SubToxin = tipsA2$ToxinSubtype
 )
-
-p <- gheatmap(
-  ggtree(tree, branch.length = "none", layout = "circular"),
-  tipsA2, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin A"
-) +
-  scale_fill_manual(
-    name = "Toxin A",
-    values = c(cols[c(2, 4, 6, 8, 10)], "#000000")
-  ) +
-  ggtitle("Toxin A - Low Stringency")
-
-ggsave("figs/ToxinA_LowStringency.pdf", p)
 
 # TOXIN B - HIGH STRINGENCY -------------------------------------------------
 
@@ -94,18 +71,6 @@ tipsB1 <- data.frame(
   SubToxin = tipsB1$ToxinSubtype
 )
 
-p <- gheatmap(
-  ggtree(tree, branch.length = "none", layout = "circular"),
-  tipsB1, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin B"
-) +
-  scale_fill_manual(
-    name = "Toxin B",
-    values = c(cols[1:9], "#000000", cols[10], "#999999")
-  ) +
-  ggtitle("Toxin B - High Stringency")
-
-ggsave("figs/ToxinB_HighStringency.pdf", p)
-
 # TOXIN B - LOW STRINGENCY -------------------------------------------------
 
 tipsB2 <- md2B
@@ -116,19 +81,24 @@ tipsB2 <- data.frame(
   SubToxin = tipsB2$ToxinSubtype
 )
 
-p <- gheatmap(
-  ggtree(tree, branch.length = "none", layout = "circular"),
-  tipsB2, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin B"
-) +
-  scale_fill_manual(
-    name = "Toxin B",
-    values = c(cols[1:9], "#000000", cols[10])
-  ) +
-  ggtitle("Toxin B - Low Stringency")
-
-ggsave("figs/ToxinB_LowStringency.pdf", p)
-
 # TOXIN A + B - HIGH STRINGENCY -------------------------------------------------
+
+tipsA1[tipsA1 == "Incomplete"] <- "Partial"
+tipsB1[tipsB1 == "Incomplete"] <- "Partial"
+
+# mdd <- md[, c("Strain", "Assembly")]
+# mdd$Assembly <- gsub("GCF", "GCA", mdd$Assembly)
+# mdd <- mdd[mdd$Assembly %in% tree$tip.label, ]
+# mdd$ToxinASubtype <- tipsA1[mdd$Assembly, , drop = TRUE]
+# mdd$ToxinBSubtype <- tipsB1[mdd$Assembly, , drop = TRUE]
+# mdd <- rbind(mdd,
+#   data.frame(Strain = NA, Assembly = tree$tip.label[!tree$tip.label %in% mdd$Assembly],
+#     ToxinASubtype = tipsA1[tree$tip.label[!tree$tip.label %in% mdd$Assembly],, drop = TRUE],
+#     ToxinBSubtype = tipsB1[tree$tip.label[!tree$tip.label %in% mdd$Assembly],, drop = TRUE]
+#   )
+# )
+# # mdd <- mdd[!is.na(mdd$Strain), ]
+# readr::write_tsv(mdd, "AssemblyToStrain2.tsv")
 
 p <- gheatmap(
   ggtree(tree, branch.length = "none", layout = "circular"),
@@ -136,7 +106,7 @@ p <- gheatmap(
 ) +
   scale_fill_manual(
     name = "Toxin A",
-    values = c(cols[c(2, 4, 6, 8, 10)], "#000000", "#999999")
+    values = c(cols[c(1:3, 8)], "gray90", "white")
   )
 
 p <- gheatmap(
@@ -145,7 +115,8 @@ p <- gheatmap(
 ) +
   scale_fill_manual(
     name = "Toxin B",
-    values = c(cols[1:9], "#000000", cols[10], "#999999")
+    breaks = c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "Partial", NA),
+    values = c(cols[c(1, 8, 12, 9, 10, 6, 2, 4, 5, 7, 3, 11)], "gray90", "white")
   ) +
   ggtitle("Toxin A + B - High Stringency")
 
@@ -153,24 +124,58 @@ ggsave("figs/ToxinAB_HighStringency.pdf", p)
 
 # TOXIN A + B - LOW STRINGENCY -------------------------------------------------
 
-p <- gheatmap(
-  ggtree(tree, branch.length = "none", layout = "circular"),
-  tipsA2, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin A"
-) +
-  scale_fill_manual(
-    name = "Toxin A",
-    values = c(cols[c(2, 4, 6, 8, 10)], "#000000", "#999999")
-  )
+# tipsA2$SubToxin <- factor(tipsA2$SubToxin)
+# tipsB2$SubToxin <- factor(tipsB2$SubToxin)
+# tipsB2$SubToxin <- factor(tipsB2$SubToxin, levels = gtools::mixedsort(levels(tipsB2$SubToxin)))
 
-p <- gheatmap(
-  p + new_scale_fill(),
-  tipsB2, offset = 5, width = 0.1, colnames = FALSE, legend_title = "Toxin B"
-) +
-  scale_fill_manual(
-    name = "Toxin B",
-    values = c(cols[1:9], "#000000", cols[10], "#999999")
-  ) +
-  ggtitle("Toxin A + B - High Stringency")
+# p <- ggtree(tree, branch.length = "none", layout = "circular")
+# qq <- readr::read_tsv("./QueryTable.tsv")
+# qq$TheLabel <- 1:nrow(qq)
+# qq <- dplyr::left_join(qq, md2, by = c(Assembly = "Assembly"))
+# p$data <- dplyr::left_join(p$data, qq, by = c(label = "Assembly"))
+# p$data$ToLabel <- !is.na(p$data$ToxinType)
 
-ggsave("figs/ToxinAB_LowStringency.pdf", p)
+# ToLabel <- c("GCA_000009205", "GCA_001995155", "GCA_000027105", "GCA_900243125", "GCA_900243285", "GCA_900243235")
+# ToLabel2 <- c("630", "VPI 10463", "R20291", "51680", "SUC36", "CD08-070")
+# p$data$ToLabel <- NA_character_
+# for (i in seq_along(ToLabel)) {
+#   p$data$ToLabel[p$data$label == ToLabel[i]] <- ToLabel2[i]
+# }
+
+# mdd <- md[, c("Strain", "Assembly")]
+# mdd$Assembly <- gsub("GCF", "GCA", mdd$Assembly)
+# mdd <- mdd[mdd$Assembly %in% tree$tip.label, ]
+# mdd$ToxinASubtype <- tipsA2[mdd$Assembly, , drop = TRUE]
+# mdd$ToxinBSubtype <- tipsB2[mdd$Assembly, , drop = TRUE]
+# mdd <- mdd[!is.na(mdd$Strain), ]
+# readr::write_tsv(mdd, "AssemblyToStrain.tsv")
+
+# p <- p + geom_tippoint(aes(subset = ToLabel), colour = "red")
+# p <- p + geom_tiplab(aes(label = TheLabel, subset = ToLabel, angle = angle), offset = 0.5, size = 3)
+# p <- p + ggrepel::geom_label_repel(aes(label = ToLabel))
+# p <- p + geom_label(aes(label = ToLabel))
+# p
+
+# p <- gheatmap(
+#   p,
+#   tipsA2, offset = 0.5, width = 0.1, colnames = FALSE, legend_title = "Toxin A"
+# ) +
+#   scale_fill_manual(
+#     name = "Toxin A",
+#     breaks = c("A1", "A2", "A3", "A6", "A7", "Incomplete", NA),
+#     values = c(cols[c(1:3, 8, 5)], "gray90", "white")
+#   )
+#
+# p <- gheatmap(
+#   p + new_scale_fill(),
+#   tipsB2, offset = 5, width = 0.1, colnames = FALSE, legend_title = "Toxin B"
+# ) +
+#   scale_fill_manual(
+#     name = "Toxin B",
+#     breaks = c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "Incomplete", NA),
+#     values = c(cols[c(1, 8, 12, 9, 10, 6, 2, 4, 5, 7, 3, 11)], "gray90", "white")
+#   ) +
+#   ggtitle("Toxin A + B - Low Stringency")
+#
+# ggsave("figs/ToxinAB_LowStringency.pdf", p)
 
